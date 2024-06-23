0e6dc9980138b3c4e9fc793330eba8ea8a3e00ff
Dize dev <axiozweb2022@gmail.com>
2024-06-22 23:54:31 -0500

    se formateo un poco el codigo

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 const fileCompletedStatus = document.querySelector('.file-compled-status');
 const fileBrowserButton = document.querySelector('.file-browse-button');
 const filebrowseInput = document.querySelector('.file-browse-input');
@@ -9,12 +8,12 @@ let totalFile = 0;
 let completFile = 0;
 
 const createFileItemHTM = (file, uiniqueIndentifier) => {
-    //extracting file name , size and extencion
-    const { name, size } = file;
+	//extracting file name , size and extencion
+	const { name, size } = file;
 
-    const extencion = name.split('.').pop();
-    //generate html for file item
-    return `
+	const extencion = name.split('.').pop();
+	//generate html for file item
+	return `
         <li class="file-item" id="file-item-${uiniqueIndentifier}">
                 <div class="file-extension">${extencion}</div>
                 <div class="file-content-wrapper">
@@ -39,97 +38,121 @@ const createFileItemHTM = (file, uiniqueIndentifier) => {
 
 //Function to hanlde file uploading
 const handleFileUploading = (file, uiniqueIndentifier) => {
-    const xhr = new XMLHttpRequest();
-    const formData = new FormData();
-    const APLI_URL = 'https://api.cloudinary.com/v1_1/dvxkqpj2v/upload';
-    formData.append('file', file);
+	const xhr = new XMLHttpRequest();
+	const formData = new FormData();
+	//const APLI_URL = '../server/app';
+	formData.append('file', file);
 
-    xhr.upload.addEventListener('progress', event => {
-        console.log(event.loaded);
-        //uploading progres bar and file size element
-        const fileProgres = document.querySelector(`#file-item-${uiniqueIndentifier} .file-progress`);
-        const fileZise = document.querySelector(`#file-item-${uiniqueIndentifier} .file-size`);
+	xhr.upload.addEventListener('progress', (event) => {
+		console.log(event.loaded);
+		//uploading progres bar and file size element
+		const fileProgres = document.querySelector(
+			`#file-item-${uiniqueIndentifier} .file-progress`,
+		);
+		const fileZise = document.querySelector(
+			`#file-item-${uiniqueIndentifier} .file-size`,
+		);
 
-        //formating the uploading or total size into KB or MB accordingly 
-        const formattedFileSize = file.size >= 1024 * 1024 ?
-            `${(event.loaded / (1024 * 1024)).toFixed(2)} MB / ${(event.total / (1024 * 1024)).toFixed(2)} MB` :
-            `${(event.loaded / 1024).toFixed(2)} KB / ${(event.total / 1024).toFixed(2)} KB`;
+		//formating the uploading or total size into KB or MB accordingly
+		const formattedFileSize =
+			file.size >= 1024 * 1024
+				? `${(event.loaded / (1024 * 1024)).toFixed(2)} MB / ${(
+						event.total /
+						(1024 * 1024)
+				  ).toFixed(2)} MB`
+				: `${(event.loaded / 1024).toFixed(2)} KB / ${(
+						event.total / 1024
+				  ).toFixed(2)} KB`;
 
-        const progress = Math.round((event.loaded / event.total) * 100);
+		const progress = Math.round((event.loaded / event.total) * 100);
 
-        fileProgres.style.width = `${progress}%`;
-        fileZise.textContent = formattedFileSize;
-    });
-    //opening connection to the server api endpoint and sendign the form data
-    xhr.open('POST', `https://api.cloudinary.com/v1_1/dvxkqpj2v/image/upload`, true);
-    xhr.send(formData);
-    return xhr;
+		fileProgres.style.width = `${progress}%`;
+		fileZise.textContent = formattedFileSize;
+	});
+	//opening connection to the server api endpoint and sendign the form data
+	xhr.open('POST', 'api.php', true);
+	xhr.send(formData);
+	return xhr;
 };
 
 //Function to handle selected file
 const handleSelectedFile = ([...files]) => {
-    if (files.length === 0) return; // cheked if no file are selected
-    totalFile += files.length;
-    files.map((file, i) => {
-        const uiniqueIndentifier = Date.now() + i;
+	if (files.length === 0) return; // cheked if no file are selected
+	totalFile += files.length;
+	files.map((file, i) => {
+		const uiniqueIndentifier = Date.now() + i;
 
-        const fileHTML = createFileItemHTM(file, uiniqueIndentifier);
-        //inserting each file item into lisr
-        filesList.insertAdjacentHTML('afterbegin', fileHTML);
-        const currentFileItem = document.querySelector(`#file-item-${uiniqueIndentifier}`);
-        const cancelFileUpload = currentFileItem.querySelector('.cancel-button');
-        const xhr = handleFileUploading(file, uiniqueIndentifier);
+		const fileHTML = createFileItemHTM(file, uiniqueIndentifier);
+		//inserting each file item into lisr
+		filesList.insertAdjacentHTML('afterbegin', fileHTML);
+		const currentFileItem = document.querySelector(
+			`#file-item-${uiniqueIndentifier}`,
+		);
+		const cancelFileUpload =
+			currentFileItem.querySelector('.cancel-button');
+		const xhr = handleFileUploading(file, uiniqueIndentifier);
 
-        xhr.addEventListener('readystatechange', () => {
+		xhr.addEventListener('readystatechange', () => {
+			if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+				completFile++;
+				cancelFileUpload.remove();
+				currentFileItem.querySelector(
+					'.file-status',
+				).textContent = `Completed`;
+				currentFileItem.querySelector(
+					'.file-status',
+				).style.color = `#00b125`;
+				fileCompletedStatus.textContent = `${completFile} / ${totalFile} Fails Completd`;
+			} else {
+				console.log('no se pudo subir al servidor');
+			}
+		});
 
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                completFile++;
-                cancelFileUpload.remove();
-                currentFileItem.querySelector('.file-status').textContent = `Completed`;
-                currentFileItem.querySelector('.file-status').style.color = `#00b125`;
-                fileCompletedStatus.textContent = `${completFile} / ${totalFile} Fails Completd`;
-            } else {
-                console.log('no se pudo subir al servidor');
-            };
-        });
-
-        //handling cancellation of file upload
-        cancelFileUpload.addEventListener('click', () => {
-            console.log('cancelando');
-            xhr.abort(); // cancelled file 
-            currentFileItem.querySelector('.file-status').textContent = `Cancelled`;
-            currentFileItem.querySelector('.file-status').style.color = `#e3413f`;
-            cancelFileUpload.remove();
-        });
-        xhr.addEventListener('error', () => {
-            alert('An error ocurred dring the file upload!');
-        });
-    });
-    fileCompletedStatus.textContent = `${completFile} / ${totalFile} Fails Completd`;
-
+		//handling cancellation of file upload
+		cancelFileUpload.addEventListener('click', () => {
+			console.log('cancelando');
+			xhr.abort(); // cancelled file
+			currentFileItem.querySelector(
+				'.file-status',
+			).textContent = `Cancelled`;
+			currentFileItem.querySelector(
+				'.file-status',
+			).style.color = `#e3413f`;
+			cancelFileUpload.remove();
+		});
+		xhr.addEventListener('error', () => {
+			alert('An error ocurred dring the file upload!');
+		});
+	});
+	fileCompletedStatus.textContent = `${completFile} / ${totalFile} Fails Completd`;
 };
 
 //Function to handle file drop event
-fileUploadBox.addEventListener('drop', event => {
-    event.preventDefault();
-    handleSelectedFile(event.dataTransfer.files);
-    fileUploadBox.classList.remove('active');
-    fileUploadBox.querySelector('.file-instruction').textContent = 'Drag files here or';
+fileUploadBox.addEventListener('drop', (event) => {
+	event.preventDefault();
+	handleSelectedFile(event.dataTransfer.files);
+	fileUploadBox.classList.remove('active');
+	fileUploadBox.querySelector('.file-instruction').textContent =
+		'Drag files here or';
 });
 
-//Function to handle file dragover event 
-fileUploadBox.addEventListener('dragover', event => {
-    event.preventDefault();
-    fileUploadBox.classList.add('active');
-    fileUploadBox.querySelector('.file-instruction').textContent = 'Release to upload or';
+//Function to handle file dragover event
+fileUploadBox.addEventListener('dragover', (event) => {
+	event.preventDefault();
+	fileUploadBox.classList.add('active');
+	fileUploadBox.querySelector('.file-instruction').textContent =
+		'Release to upload or';
 });
 
 //Function to handle file dragleave event
-fileUploadBox.addEventListener('dragleave', event => {
-    event.preventDefault();
-    fileUploadBox.classList.remove('active');
-    fileUploadBox.querySelector('.file-instruction').textContent = 'Drag files here or';
+fileUploadBox.addEventListener('dragleave', (event) => {
+	event.preventDefault();
+	fileUploadBox.classList.remove('active');
+	fileUploadBox.querySelector('.file-instruction').textContent =
+		'Drag files here or';
 });
 
-filebrowseInput.addEventListener('change', event => handleSelectedFile(event.target.files));
+filebrowseInput.addEventListener('change', (event) =>
+	handleSelectedFile(event.target.files),
+);
 fileBrowserButton.addEventListener('click', () => filebrowseInput.click());
